be6d7522f40cac184fdd251ed48e043a2c7ec018
Shail <shailapps@gmail.com>
2022-01-27 20:02:50 +0530

    added AMI in terraform_1

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_example" {
-    ami = "ami-01fc429821bf1f4b4"  
+    ami = "ami-0b0af3577fe5e3532"  
     instance_type = "t2.micro" 
     tags = {
         Name = "Terraform EC2"
