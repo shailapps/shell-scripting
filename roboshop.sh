76e0f9155846be76681d96783cc5af8f8d07b570
Shailesh Gudimalla <shailapps@gmail.com>
2022-01-30 15:49:48 +0530

    k8s

--- roboshop.sh
+++ roboshop.sh
@@ -308,6 +308,17 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" >/tmp/reset.sql
     Stat_Check $?
     ;;
 
+k8s)
+    Print "Install k8s : Kubernetes "
+    curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl  &>>$LOG_FILE
+    openssl sha1 -sha256 kubectl
+    chmod +x ./kubectl
+    mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+    echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+    kubectl version --short --client
+    Stat_Check $?
+;;
+
   rabbitmq)
 
     Print "Install RabbitMQ\t"
